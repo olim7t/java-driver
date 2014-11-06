9454a364de5d35b974e6a22140e8ceaaafb52a60
olim7t <omichallat+github@gmail.com>
2014-11-06 11:28:40 +0100

    Make SchemaStatement extend RegularStatement.

--- driver-core/src/main/java/com/datastax/driver/core/schemabuilder/SchemaStatement.java
+++ driver-core/src/main/java/com/datastax/driver/core/schemabuilder/SchemaStatement.java
@@ -15,21 +15,57 @@
  */
 package com.datastax.driver.core.schemabuilder;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 import com.datastax.driver.core.DataType;
+import com.datastax.driver.core.ProtocolVersion;
+import com.datastax.driver.core.RegularStatement;
+
 import com.google.common.base.Strings;
 
-public abstract class SchemaStatement {
+public abstract class SchemaStatement extends RegularStatement {
 
     static final String STATEMENT_START = "\n\t";
     static final String COLUMN_FORMATTING = "\n\t\t";
 
     static final List<String> RESERVED_KEYWORDS = Arrays.asList("add,allow,alter,and,any,apply,asc,authorize,batch,begin,by,columnfamily,create,delete,desc,drop,each_quorum,from,grant,in,index,inet,infinity,insert,into,keyspace,keyspaces,limit,local_one,local_quorum,modify,nan,norecursive,of,on,order,password,primary,quorum,rename,revoke,schema,select,set,table,three,to,token,truncate,two,unlogged,update,use,using,where,with".split(","));
 
+    private volatile String cache;
+
     abstract String buildInternal();
+
+    @Override
+    public String getQueryString() {
+        if (cache == null)
+            cache = buildInternal();
+        return cache;
+    }
+
+    @Override
+    public ByteBuffer[] getValues(ProtocolVersion protocolVersion) {
+        // DDL statements never have values
+        return new ByteBuffer[0];
+    }
+
+    @Override
+    public boolean hasValues() {
+        return false;
+    }
+
+    @Override
+    public String getKeyspace() {
+        // This is exposed for token-aware routing. Since there is no token awareness for DDL statements, we don't need to
+        // return anything here (even if a keyspace has been explicitly set in the statement).
+        return null;
+    }
+
+    @Override
+    public ByteBuffer getRoutingKey() {
+        return null; // there is no token awareness for DDL statements
+    }
 
     protected static void validateNotEmpty(String columnName, String label) {
         if (Strings.isNullOrEmpty(columnName)) {
